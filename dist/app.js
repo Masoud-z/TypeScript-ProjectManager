05ba09c3fd2f9b0e60d0f207d3c8fe15676c7828
Masoud <masoud.zeinalabedini@gmail.com>
2023-09-13 17:56:10 +0330

    create ProjectList and create an instance of it

--- dist/app.js
+++ dist/app.js
@@ -43,6 +43,28 @@ function autoBind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
+// ProjectList class
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById("project-list");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector("ul").id = listId;
+        this.element.querySelector("h2").textContent =
+            this.type.toUpperCase() + " PROJECTS";
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
 // ProjectInput Class
 class ProjectInput {
     constructor() {
@@ -96,7 +118,6 @@ class ProjectInput {
         const userInput = this.gatherUserInput();
         if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
-            // console.log(title, desc, people);
             this.clearInputs();
         }
     }
@@ -111,4 +132,6 @@ __decorate([
     autoBind
 ], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishPrjList = new ProjectList("finished");
 //# sourceMappingURL=app.js.map
